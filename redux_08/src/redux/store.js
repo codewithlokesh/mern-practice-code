984f94fa289e0efcd1a8ca32520c41b3f83fba3a
lokesh <inglelokesh20@gmail.com>
2026-02-25 00:22:26 +0530

    Removed unnecessary folder

--- redux_08/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice.js";
-import todoReducer from "./todoSlice.js"
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        todos: todoReducer
-    },
-})
